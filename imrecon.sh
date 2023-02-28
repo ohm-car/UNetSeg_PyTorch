c5fec1791f7b175d1943f0509a777cdcb3fdbf5d
omkar-umbc <omkark1@umbc.edu>
2023-02-27 19:19:34 -0500

    Pets Segmentation UNet with Image Recon Files

--- imrecon.sh
+++ imrecon.sh
@@ -4,6 +4,7 @@
 #SBATCH --mail-user=omkark1@umbc.edu
 #SBATCH --mem=32G
 #SBATCH --gres=gpu:1
+#SBATCH --gres=cpu:4
 #SBATCH --time=10-12:00:00
 #SBATCH --constraint=rtx_6000
 #SBATCH --output=/nfs/ada/oates/users/omkark1/Thesis_Work/UNetSeg_PyTorch/outfiles/imrecon_run_1.out
