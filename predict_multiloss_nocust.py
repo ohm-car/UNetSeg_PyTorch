9f3701554e0df0cfddbcea26e59eaace1108028d
omkar-umbc <omkark1@umbc.edu>
2023-04-20 09:53:34 -0400

    multiloss regular segmentation

---
+++ predict_multiloss_nocust.py
@@ -0,0 +1,170 @@
+import argparse
+import logging
+import os
+
+import numpy as np
+import torch
+import torch.nn.functional as F
+from PIL import Image
+from torchvision import transforms
+
+from unet import UNet
+from utils.data_vis import plot_img_and_mask
+from utils.petsReconDataset import PetsReconDataset
+
+
+def predict_img(net,
+                full_img,
+                device,
+                scale_factor=1,
+                out_threshold=0.5):
+    net.eval()
+
+    img = torch.from_numpy(PetsReconDataset.preprocess(full_img, scale_factor, isImage=True))
+
+    img = img.unsqueeze(0)
+    img = img.to(device=device, dtype=torch.float32)
+
+    with torch.no_grad():
+        pred_im, pred_mask = net(img)
+
+        print('ReconIM shape:', pred_im.shape)
+        print('Mask shape:', pred_mask.shape)
+
+        # if net.n_classes > 1:
+        #     im_probs = F.softmax(output, dim=1)
+        # else:
+        #     im_probs = torch.sigmoid(output)
+
+        im_probs = pred_im.squeeze(0)
+        mask_probs = pred_mask.squeeze(0)
+
+
+        tf = transforms.Compose(
+            [
+                transforms.ToPILImage(),
+                transforms.Resize(full_img.size[1]),
+                transforms.ToTensor()
+            ]
+        )
+
+        im_probs = tf(im_probs.cpu())
+        mask_probs = tf(mask_probs.cpu())
+        full_im = im_probs.squeeze().cpu().numpy()
+        print('mask_probs shape:', mask_probs.shape)
+        full_mask = mask_probs.squeeze().cpu().numpy()
+
+    return full_im, full_mask
+
+
+def get_args():
+    parser = argparse.ArgumentParser(description='Predict masks from input images',
+                                     formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument('--model', '-m', default='MODEL.pth',
+                        metavar='FILE',
+                        help="Specify the file in which the model is stored")
+    parser.add_argument('--input', '-i', metavar='INPUT', nargs='+',
+                        help='filenames of input images', required=True)
+
+    parser.add_argument('--output', '-o', metavar='INPUT', nargs='+',
+                        help='Filenames of ouput images')
+    parser.add_argument('--viz', '-v', action='store_true',
+                        help="Visualize the images as they are processed",
+                        default=False)
+    parser.add_argument('--no-save', '-n', action='store_true',
+                        help="Do not save the output masks",
+                        default=False)
+    parser.add_argument('--mask-threshold', '-t', type=float,
+                        help="Minimum probability value to consider a mask pixel white",
+                        default=0.5)
+    parser.add_argument('--scale', '-s', type=float,
+                        help="Scale factor for the input images",
+                        default=0.5)
+
+    return parser.parse_args()
+
+
+def get_output_filenames(args):
+    in_files = args.input
+    out_files = []
+
+    if not args.output:
+        for f in in_files:
+            pathsplit = os.path.splitext(f)
+            # print(pathsplit)
+            out_files.append(("{}_OUT{}".format(pathsplit[0], pathsplit[1]), "{}_OUT_M{}".format(pathsplit[0], pathsplit[1])))
+    elif len(in_files) != len(args.output):
+        logging.error("Input files and output files are not of the same length")
+        raise SystemExit()
+    else:
+        out_files = args.output
+
+    return out_files
+
+
+# def mask_to_image(mask):
+#     return Image.fromarray((mask * 255).astype(np.uint8))
+
+def imrecon_to_image(mask):
+    
+    # print(mask, mask * 255)
+    mask = mask.transpose((1,2,0))
+    # print(mask.shape)
+    return Image.fromarray((mask * 255).astype(np.uint8), 'RGB')
+
+def mask_to_image(mask):
+    
+    print(mask.shape, type(mask))
+    # mask = mask.transpose((1,2,0))
+    # print(mask.shape, type(mask))
+    return Image.fromarray((mask * 255).astype(np.uint8), '1')
+
+
+if __name__ == "__main__":
+    args = get_args()
+    in_files = args.input
+    out_files = get_output_filenames(args)
+
+    net = UNet(n_channels=3, n_classes=1)
+
+    logging.info("Loading model {}".format(args.model))
+
+    # device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    if torch.cuda.is_available():
+        device = torch.device('cuda')
+    elif torch.backends.mps.is_available():
+        device = torch.device('mps')
+    else:
+        device = torch.device('cpu')
+    logging.info(f'Using device {device}')
+    net.to(device=device)
+    net.load_state_dict(torch.load(args.model, map_location=device))
+
+    logging.info("Model loaded !")
+
+    for i, fn in enumerate(in_files):
+        logging.info("\nPredicting image {} ...".format(fn))
+
+        img = Image.open(fn)
+
+        rec_im, mask = predict_img(net=net,
+                           full_img=img,
+                           scale_factor=args.scale,
+                           out_threshold=args.mask_threshold,
+                           device=device)
+
+        # print(type(mask), mask.shape)
+        # mask = np.argmax(mask, axis = 0)
+        print(mask.shape)
+        if not args.no_save:
+            out_fn = out_files[i]
+            result_im = imrecon_to_image(rec_im)
+            result_mask = mask_to_image(mask)
+            result_im.save(out_files[i][0])
+            result_mask.save(out_files[i][1])
+
+            logging.info("Mask saved to {}".format(out_files[i]))
+
+        if args.viz:
+            logging.info("Visualizing results for image {}, close to continue ...".format(fn))
+            plot_img_and_mask(img, mask)
---
+++ utils/petsReconSegDataset.py
@@ -0,0 +1,118 @@
+from os.path import splitext
+from os import listdir
+import numpy as np
+from glob import glob
+import torch
+from torch.utils.data import Dataset
+import logging
+import PIL
+from PIL import Image, ImageOps
+from torch.nn.functional import one_hot
+
+"""A custom dataset loader object. This dataset returns the same labels as the input"""
+
+class PetsReconDataset(Dataset):
+    def __init__(self, imgs_dir, masks_dir, scale=1, mask_suffix=''):
+        self.imgs_dir = imgs_dir
+        self.masks_dir = masks_dir
+        self.scale = scale
+        self.mask_suffix = mask_suffix
+        assert 0 < scale <= 1, 'Scale must be between 0 and 1'
+
+        self.ids = [splitext(file)[0] for file in listdir(imgs_dir)
+                    if not file.startswith('.')]
+        logging.info(f'Creating dataset with {len(self.ids)} examples')
+
+    def __len__(self):
+        return len(self.ids)
+
+
+    @classmethod
+    def preprocess(cls, pil_img, scale, isImage):
+        w, h = pil_img.size
+        newW, newH = int(scale * w), int(scale * h)
+        assert newW > 0 and newH > 0, 'Scale is too small'
+        pil_img = pil_img.resize((160, 160))
+
+        img_nd = np.array(pil_img)
+
+        if len(img_nd.shape) == 2:
+            img_nd -= 1
+            # img_nd = np.expand_dims(img_nd, axis=2)
+
+        # if not isImage:
+            # img_nd = cls.onehot_initialization(cls, img_nd)
+            # img_nd -= 1
+            # img_nd = (np.arange(img_nd.max()+1) == img_nd[...,None]).astype(int)
+            # print(img_nd.shape)
+
+        # HWC to CHW
+        
+        if isImage:
+            img_trans = img_nd.transpose((2, 0, 1))
+            if img_trans.max() > 1:
+                img_trans = img_trans / 255
+        else:
+            img_trans = img_nd
+
+        return img_trans
+
+    # def processMask(self, pilmask):
+
+    #     mask = np.asarray(pilmask)
+
+    #     mask = np.sum(mask, axis = 2)
+    #     mask = mask == 765
+
+    #     return Image.fromarray(np.uint8(mask))
+
+    # def all_idx(self, idx, axis):
+    #     grid = np.ogrid[tuple(map(slice, idx.shape))]
+    #     grid.insert(axis, idx)
+    #     return tuple(grid)
+
+    # def onehot_initialization(self, a):
+    #     ncols = a.max()+1
+    #     out = np.zeros(a.shape + (ncols,), dtype=int)
+    #     out[self.all_idx(a, axis=2)] = 1
+    #     return out
+
+    def __getitem__(self, i):
+        idx = self.ids[i]
+        # print(self.imgs_dir, self.masks_dir, self.mask_suffix)
+        mask_file = glob(self.masks_dir + idx + self.mask_suffix + '.*')
+        # print(mask_file)
+        img_file = glob(self.imgs_dir + idx + '.*')
+        # print(img_file)
+
+        assert len(mask_file) == 1, \
+            f'Either no mask or multiple masks found for the ID {idx}: {mask_file}'
+        assert len(img_file) == 1, \
+            f'Either no image or multiple images found for the ID {idx}: {img_file}'
+        mask = Image.open(mask_file[0])
+        # print(mask.size, mask.mode)
+        # mask = self.processMask(mask)
+        img = Image.open(img_file[0])
+        if img.mode != 'RGB':
+            # print(img_file, img.mode)
+            img = img.convert(mode = 'RGB')
+            # print(img_file, img.mode)
+        # print(img.size)
+        # print(type(mask))
+
+        assert img.size == mask.size, \
+            f'Image and mask {idx} should be the same size, but are {img.size} and {mask.size}'
+
+        img = self.preprocess(img, self.scale, isImage = True)
+        mask = self.preprocess(mask, self.scale, isImage = False)
+
+        return {
+            'image': torch.from_numpy(img).type(torch.FloatTensor),
+            'reconstructed_image': torch.from_numpy(img).type(torch.FloatTensor)
+        }
+
+
+class CarvanaDataset(PetsReconDataset):
+    def __init__(self, imgs_dir, masks_dir, scale=1):
+        # super().__init__(imgs_dir, masks_dir, scale, mask_suffix='_mask')
+        super().__init__(imgs_dir, masks_dir, scale, mask_suffix='')
