ff664da5e997390b663aa8cd66d365b6010ec897
omkar-umbc <omkark1@umbc.edu>
2023-05-18 12:48:44 -0400

    Correcting code for individual train

--- train_multiloss_nocust.py
+++ train_multiloss_nocust.py
@@ -99,17 +99,18 @@ def train_net(net,
                 # print(torch.mean(torch.squeeze(pred_mask), (1,2)).shape, true_mask)
                 # print("pred_mask shape:", pred_mask.shape, "true_mask shape:", true_mask.shape)
                 mask_loss = criterion(pred_mask, true_mask)
-                total_loss = recon_loss + mask_loss
+                # total_loss = recon_loss + mask_loss
                 # total_loss = mask_loss
+                total_loss = recon_loss
                 epoch_loss += recon_loss.item() + mask_loss.item()
                 writer.add_scalar('total_loss/train', total_loss.item(), global_step)
 
-                pbar.set_postfix(**{'mask loss (batch)': mask_loss.item(), 'reconstruction recon_loss': recon_loss.item(),'total loss (batch)': total_loss.item()})
+                pbar.set_postfix(**{'mask loss (batch)': mask_loss.item(), 'reconstruction loss': recon_loss.item(),'total loss (batch)': total_loss.item()})
 
                 optimizer.zero_grad()
-                # total_loss.backward()
+                total_loss.backward()
                 # mask_loss.backward()
-                recon_loss.backward()
+                # recon_loss.backward()
                 nn.utils.clip_grad_value_(net.parameters(), 0.1)
                 optimizer.step()
 
