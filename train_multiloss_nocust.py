8ef05117d8462c22a49bb149608e0e10f2cc3380
Omkar Kulkarni <omkar@Omkars-MacBook-Pro.local>
2023-05-27 14:32:52 -0400

    Possible bugfix

--- train_multiloss_nocust.py
+++ train_multiloss_nocust.py
@@ -67,8 +67,8 @@ def train_net(net,
     else:
         criterion = nn.BCEWithLogitsLoss()
     
-    logging.info("Testing with both mask and recon losses")
-    # logging.info("Testing with keeping just reconstruction loss on")
+    # logging.info("Testing with both mask and recon losses")
+    logging.info("Testing with keeping just reconstruction loss on")
     # logging.info("Testing with keeping just mask loss on")
 
     for epoch in range(epochs):
@@ -103,9 +103,9 @@ def train_net(net,
                 # print(torch.mean(torch.squeeze(pred_mask), (1,2)).shape, true_mask)
                 # print("pred_mask shape:", pred_mask.shape, "true_mask shape:", true_mask.shape)
                 mask_loss = criterion(pred_mask, true_mask)
-                total_loss = recon_loss + mask_loss
+                # total_loss = recon_loss + mask_loss
                 # total_loss = mask_loss
-                # total_loss = recon_loss
+                total_loss = recon_loss
                 epoch_loss += recon_loss.item() + mask_loss.item()
                 writer.add_scalar('total_loss/train', total_loss.item(), global_step)
 
@@ -122,7 +122,7 @@ def train_net(net,
                 pbar.update(imgs.shape[0])
                 global_step += 1
                 # print(global_step, n_train, batch_size)
-                if global_step % (n_train // (1 * batch_size) + 1) == 0:
+                if global_step % (n_train // (10 * batch_size) + 1) == 0:
                     for tag, value in net.named_parameters():
                         tag = tag.replace('.', '/')
                         writer.add_histogram('weights/' + tag, value.data.cpu().numpy(), global_step)
