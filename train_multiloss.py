c7f9cce9d4d66f9067e365ecc4efe766d70af4a7
Omkar Kulkarni <omkar@Omkars-MacBook-Pro.local>
2023-07-14 15:58:09 -0400

    Extending mask branch

--- train_multiloss.py
+++ train_multiloss.py
@@ -159,7 +159,7 @@ def get_args():
                         help='Number of epochs', dest='epochs')
     parser.add_argument('-sf', '--saveFreq', metavar='SF', type=int, default=1,
                         help='Save every sf epochs', dest='saveFreq')
-    parser.add_argument('-b', '--batch-size', metavar='B', type=int, nargs='?', default=2,
+    parser.add_argument('-b', '--batch-size', metavar='B', type=int, nargs='?', default=4,
                         help='Batch size', dest='batchsize')
     parser.add_argument('-l', '--learning-rate', metavar='LR', type=float, nargs='?', default=0.0001,
                         help='Learning rate', dest='lr')
--- unet/__init__.py
+++ unet/__init__.py
@@ -1,5 +1,6 @@
 # from .unet_model import UNet
 # from .unet_model_seg import UNet
 # from .unet_model_multiloss import UNet
-from .unet_model_multiloss_nocust import UNet
+# from .unet_model_multiloss_nocust import UNet
 # from .unet_model_recon import UNet
+from .unet_model_x import UNet
