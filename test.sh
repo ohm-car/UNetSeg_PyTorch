44363c033500809c09e11000dbc544e223f4a7bd
omkar-umbc <omkark1@umbc.edu>
2024-02-22 17:21:55 -0500

    Test Scratch

--- test.sh
+++ test.sh
@@ -19,9 +19,9 @@ source activate unet
 # python /nfs/ada/oates/users/omkark1/Thesis_Work/UNetSeg_PyTorch/train_multiloss.py -b=160 -e=60 -sf=10
 
 cp /nfs/ada/oates/users/omkark1/Thesis_Work/Datasets.zip /scratch/$SLURM_JOBID
-unzip /scratch/$SLURM_JOBID/Datasets.zip -d /scratch/$SLURM_JOBID/Datasets
+unzip /scratch/$SLURM_JOBID/Datasets.zip -d /scratch/$SLURM_JOBID
 ls /scratch/$SLURM_JOBID/Datasets/petsData/
 ls /scratch/$SLURM_JOBID/Datasets/petsData/images/
 
-mv output_$SLURM_JOB_ID.log /nfs/ada/oates/users/omkark1/Thesis_Work/UNetSeg_PyTorch/outfiles/multiloss/output_$DT.log
-mv output_$SLURM_JOB_ID.err /nfs/ada/oates/users/omkark1/Thesis_Work/UNetSeg_PyTorch/outfiles/multiloss/output_$DT.err
+# mv output_$SLURM_JOB_ID.log /nfs/ada/oates/users/omkark1/Thesis_Work/UNetSeg_PyTorch/outfiles/multiloss/output_$DT.log
+# mv output_$SLURM_JOB_ID.err /nfs/ada/oates/users/omkark1/Thesis_Work/UNetSeg_PyTorch/outfiles/multiloss/output_$DT.err
