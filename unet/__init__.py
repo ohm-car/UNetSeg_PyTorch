04465a4116b232e8f56f72cea396eb4ea15ebe7a
Omkar Kulkarni <90357608+ohm-car@users.noreply.github.com>
2024-02-23 15:43:29 -0500

    Update __init__.py

--- unet/__init__.py
+++ unet/__init__.py
@@ -4,5 +4,5 @@
 # from .unet_model_multiloss_nocust import UNet
 # from .unet_model_recon import UNet
 # from .unet_model_x import UNet
-from .unet_model_xA import UNet
-# from .unet_model_xB import UNet
+# from .unet_model_xA import UNet
+from .unet_model_xB import UNet
