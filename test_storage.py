ab28df93cde98cce7d02e39ca02f752f9405a5f3
omkar-umbc <omkark1@umbc.edu>
2024-02-24 18:12:14 -0500

    Test ADA

--- test_storage.py
+++ test_storage.py
@@ -21,11 +21,43 @@ nas_dir_img = os.path.join(nas_dir, 'Datasets/petsData/images/')
 scratch_dir = sys.argv[1]
 scratch_dir_img = os.path.join(scratch_dir, 'Datasets/petsData/images/')
 
+def preprocess(pil_img, scale, isImage):
+    w, h = pil_img.size
+    newW, newH = int(scale * w), int(scale * h)
+    assert newW > 0 and newH > 0, 'Scale is too small'
+    pil_img = pil_img.resize((160, 160))
+
+    img_nd = np.array(pil_img)
+
+    if len(img_nd.shape) == 2:
+        img_nd -= 1
+        # img_nd = np.expand_dims(img_nd, axis=2)
+
+    # if not isImage:
+        # img_nd = cls.onehot_initialization(cls, img_nd)
+        # img_nd -= 1
+        # img_nd = (np.arange(img_nd.max()+1) == img_nd[...,None]).astype(int)
+        # print(img_nd.shape)
+
+    # HWC to CHW
+    
+    if isImage:
+        img_trans = img_nd.transpose((2, 0, 1))
+        if img_trans.max() > 1:
+            img_trans = img_trans / 255
+    else:
+        img_trans = img_nd
+
+    return img_trans
+
 nas_imgs = os.listdir(nas_dir_img)
 print(str(len(nas_imgs)) + " Images found in NAS storage")
 nas_time = time.time()
 for i in nas_imgs:
 	T = Image.open(nas_dir_img + i)
+	if T.mode != 'RGB':
+		T = T.convert(mode = 'RGB')
+	T = preprocess(T, 1, True)
 nas_time = time.time() - nas_time
 print("NAS: Time taken to read " + str(len(nas_imgs)) + " images is " + str(nas_time) + " seconds.")
 
@@ -34,5 +66,8 @@ print(str(len(scatch_imgs)) + " Images found in Scratch storage")
 scratch_time = time.time()
 for i in scatch_imgs:
 	T = Image.open(scratch_dir_img + i)
+	if T.mode != 'RGB':
+		T = T.convert(mode = 'RGB')
+	T = preprocess(T, 1, True)
 scratch_time = time.time() - scratch_time
 print("Scratch: Time taken to read " + str(len(scatch_imgs)) + " images is " + str(scratch_time) + " seconds.")
