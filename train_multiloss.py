2419fbf393af0e7052df32867e971b083971264c
omkar-umbc <omkark1@umbc.edu>
2024-03-10 19:44:58 -0400

    Test

--- train_multiloss.py
+++ train_multiloss.py
@@ -70,6 +70,7 @@ def train_net(args,
     # val_loader = DataLoader(val, batch_size=batch_size, shuffle=False, pin_memory=True, drop_last=True)
 
     train_loader = DataLoader(train, batch_size=batch_size, shuffle=True, num_workers = 2)
+
     val_loader = DataLoader(val, batch_size=batch_size, shuffle=False, num_workers = 2, pin_memory=True, drop_last=True)
 
     writer = SummaryWriter(comment=f'LR_{lr}_BS_{batch_size}_SCALE_{img_scale}')
@@ -105,7 +106,11 @@ def train_net(args,
         epoch_loss = 0
         with tqdm(total=n_train, desc=f'Epoch {epoch + 1}/{epochs}', unit='img') as pbar:
             for batch in train_loader:
+<<<<<<< HEAD
                 # print("Started Batch Training!\n")
+=======
+                print('Loaded batch')
+>>>>>>> 6758522 (Update train_multiloss.py)
                 imgs = batch['image']
                 recon_img = batch['reconstructed_image']
                 imgs_percs = batch['mask_perc']
@@ -143,7 +148,11 @@ def train_net(args,
 
                 pbar.update(imgs.shape[0])
                 global_step += 1
+<<<<<<< HEAD
                 # print("Finished Batch Training!\n")
+=======
+                print('Processed batch')
+>>>>>>> 6758522 (Update train_multiloss.py)
                 # print(global_step, n_train, batch_size)
                 if global_step % (n_train // (1 * batch_size) + 1) == 0:
                 # if global_step % (n_train // (100 * batch_size) + 1) == 0:
