d431de5f18758d447b13d9165969079f4f419c45
omkar-umbc <omkark1@umbc.edu>
2024-04-30 21:13:29 -0400

    voc

--- voc_eval_seg.py
+++ voc_eval_seg.py
@@ -4,7 +4,7 @@ from tqdm import tqdm
 from utils.percLoss import percLoss
 from dice_loss import dice_coeff
 from torchmetrics.classification import BinaryJaccardIndex
-from torchmetrics.functional.classification import binary_jaccard_index
+from torchmetrics.functional.classification import binary_jaccard_index, multiclass_jaccard_index
 
 
 def eval_net(net, loader, device, regularizer):
@@ -22,7 +22,10 @@ def eval_net(net, loader, device, regularizer):
         for batch in loader:
             imgs, recon_img, true_masks, true_perc = batch['image'], batch['reconstructed_image'], batch['mask'], batch['mask_perc']
             imgs = imgs.to(device=device, dtype=torch.float32)
-            true_masks = true_masks.to(device=device, dtype=torch.long)
+            true_masks = true_masks.to(device=device, dtype=torch.float32)
+            print(true_masks.shape)
+            amax_true_masks = torch.argmax(true_masks, dim=1)
+            print(amax_true_masks.shape)
             recon_img = recon_img.to(device=device, dtype=torch.float32)
             true_perc = true_perc.to(device=device, dtype=torch.float32)
 
@@ -30,39 +33,43 @@ def eval_net(net, loader, device, regularizer):
                 pred_masks = net(imgs)
 
             if net.n_classes > 1:
-                seg_loss_batch = F.l1_loss(pred_masks, true_masks).item()
+                # seg_loss_batch = F.l1_loss(pred_masks, true_masks).item()
+                seg_loss_batch = F.cross_entropy(pred_masks, true_masks).item()
                 # pcLossCriterion = percLoss(threshold_prob = 0.9, regularizer = regularizer)
                 # mask_loss_batch = pcLossCriterion(pred_mask, true_perc).item()
 
                 mean_batch_iou = 0
                 for i in range(len(pred_masks)):
-                    single_iou = binary_jaccard_index(pred_masks[i], true_masks[i])
+                    single_iou = multiclass_jaccard_index(pred_masks[i], true_masks[i], num_classes=21)
                     # print(single_iou)
                     mean_batch_iou += single_iou
 
-                batch_iou = binary_jaccard_index(pred_masks, true_masks)
-                # print(batch_iou, mean_batch_iou / len(pred_mask), mean_batch_iou)
+                batch_iou = multiclass_jaccard_index(pred_masks, amax_true_masks, num_classes=21)
+                # print(batch_iou, mean_batch_iou / len(pred_masks), mean_batch_iou)
 
-                iou += (mean_batch_iou / len(pred_masks))
+                # iou += (mean_batch_iou / len(pred_masks))
+                iou += batch_iou
                 seg_loss += seg_loss_batch
                 # mask_loss += mask_loss_batch
                 # tot += seg_loss_batch + mask_loss_batch
                 tot += seg_loss_batch
             else:
-                seg_loss_batch = F.l1_loss(pred_masks, true_masks).item()
+                # seg_loss_batch = F.l1_loss(pred_masks, true_masks).item()
+                seg_loss_batch = F.cross_entropy(pred_masks, true_masks).item()
                 # pcLossCriterion = percLoss(threshold_prob = 0.9, regularizer = regularizer)
                 # mask_loss_batch = pcLossCriterion(pred_mask, true_perc).item()
 
                 mean_batch_iou = 0
                 for i in range(len(pred_masks)):
-                    single_iou = binary_jaccard_index(pred_masks[i], true_masks[i])
+                    single_iou = multiclass_jaccard_index(pred_masks[i], true_masks[i], num_classes=21)
                     # print(single_iou)
                     mean_batch_iou += single_iou
 
-                batch_iou = binary_jaccard_index(pred_masks, true_masks)
+                batch_iou = multiclass_jaccard_index(pred_masks, true_masks, num_classes=21)
                 # print(batch_iou, mean_batch_iou / len(pred_mask), mean_batch_iou)
 
-                iou += (mean_batch_iou / len(pred_masks))
+                # iou += (mean_batch_iou / len(pred_masks))
+                iou += batch_iou
                 seg_loss += seg_loss_batch
                 # mask_loss += mask_loss_batch
                 # tot += seg_loss_batch + mask_loss_batch
