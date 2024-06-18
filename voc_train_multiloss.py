7e36bc2e021ed2bfdb5d59a592c576896269515d
omkar-umbc <omkark1@umbc.edu>
2024-06-18 19:57:43 -0400

    voc

--- voc_train_multiloss.py
+++ voc_train_multiloss.py
@@ -13,7 +13,7 @@ from tqdm import tqdm
 import torchsummary
 import datetime
 
-from eval_multiloss import eval_net
+from voc_eval_multiloss import eval_net
 from unet import UNet
 
 from torch.utils.tensorboard import SummaryWriter
@@ -21,6 +21,8 @@ from utils.pascalVOC_multiloss_pl import PascalVOCDataset
 # from utils.petsReconDataset_multiloss import PetsReconDataset
 from utils.percLoss import percLoss
 from torch.utils.data import DataLoader, random_split
+
+from torchvision.models.segmentation import fcn_resnet50, FCN
 
 # root_dir = Path().resolve().parent
 # dir_img = os.path.join(root_dir, 'Datasets/petsData/images/')
@@ -33,6 +35,18 @@ dir_img = None
 dir_mask = None
 tm = datetime.datetime.now()
 dir_checkpoint = None
+
+def create_model():
+
+    model = fcn_resnet50(aux_loss=True)
+    aux = nn.Sequential(nn.Conv2d(1024, 512, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1), bias=False),
+                 nn.BatchNorm2d(512, eps=1e-05, momentum=0.1, affine=True, track_running_stats=True),
+                 nn.ReLU(inplace=True),
+                 nn.Dropout(p=0.1, inplace=False),
+                 nn.Conv2d(512, 3, kernel_size=(1, 1), stride=(1, 1)),
+                 nn.Sigmoid())
+    model.aux_classifier = aux
+    return model
 
 
 def train_net(args,
@@ -95,12 +109,15 @@ def train_net(args,
     # optimizer = optim.RMSprop(net.parameters(), lr=lr, weight_decay=1e-8, momentum=0.9)
     optimizer = optim.Adam(net.parameters(), lr=lr, weight_decay=1e-8)
     # scheduler = optim.lr_scheduler.ReduceLROnPlateau(optimizer, 'min' if net.n_classes > 1 else 'max', patience=2)
-    if net.n_classes > 1:
-        criterion = nn.L1Loss()
-    else:
-        criterion = nn.L1Loss()
+    # if net.n_classes > 1:
+    #     criterion = nn.L1Loss()
+    # else:
+    #     criterion = nn.L1Loss()
 
-    weight_recon_loss, weight_percLoss = 1, 5
+    criterion_recon = nn.L1Loss()
+    criterion_mask = percLoss(threshold_prob = 0.9, regularizer = regularizer, regularizer_weight = regularizer_weight, sampler = args.sp)
+
+    weight_recon_loss, weight_percLoss = 1, 1
 
     for epoch in range(epochs):
         net.train()
@@ -116,10 +133,10 @@ def train_net(args,
                 masks = batch['mask']
                 recon_img = batch['reconstructed_image']
                 imgs_percs = batch['mask_perc']
-                assert imgs.shape[1] == net.n_channels, \
-                    f'Network has been defined with {net.n_channels} input channels, ' \
-                    f'but loaded images have {imgs.shape[1]} channels. Please check that ' \
-                    'the images are loaded correctly.'
+                # assert imgs.shape[1] == net.n_channels, \
+                #     f'Network has been defined with {net.n_channels} input channels, ' \
+                #     f'but loaded images have {imgs.shape[1]} channels. Please check that ' \
+                #     'the images are loaded correctly.'
 
                 imgs = imgs.to(device=device, dtype=torch.float32)
                 masks = masks.to(device=device, dtype=torch.long)
@@ -127,16 +144,17 @@ def train_net(args,
                 recon_img = recon_img.to(device=device, dtype=torch.float32)
                 imgs_percs = imgs_percs.to(device=device, dtype=torch.float32)
 
-                pred_recon_img, pred_mask = net(imgs)
+                recmasks = net(imgs)
+                pred_recon_img, pred_mask = recmasks["aux"], recmasks["out"]
                 # pred_recon_img = torch.argmax(pred_recon_img, dim=1)
                 # print("Masks Pred shape:", pred_recon_img.shape, "True Masks shape:", recon_img.shape)
-                pcLossCriterion = percLoss(threshold_prob = 0.9, regularizer = regularizer, regularizer_weight = regularizer_weight, sampler = args.sp)
+                # pcLossCriterion = percLoss(threshold_prob = 0.9, regularizer = regularizer, regularizer_weight = regularizer_weight, sampler = args.sp)
                 # pcLossCriterion = nn.L1Loss()
 
-                loss = weight_recon_loss * criterion(pred_recon_img, recon_img)
+                loss = weight_recon_loss * criterion_recon(pred_recon_img, recon_img)
                 # print(torch.squeeze(pred_mask).shape)
                 # print(torch.mean(torch.squeeze(pred_mask), (1,2)).shape, imgs_percs)
-                pcLoss = weight_percLoss * pcLossCriterion(pred_mask, imgs_percs)
+                pcLoss = weight_percLoss * criterion_mask(pred_mask, imgs_percs)
                 total_loss = loss + pcLoss
                 epoch_loss += loss.item() + pcLoss.item()
                 writer.add_scalar('Loss/train', total_loss.item(), global_step)
@@ -163,11 +181,12 @@ def train_net(args,
                         tag = tag.replace('.', '/')
                         writer.add_histogram('weights/' + tag, value.data.cpu().numpy(), global_step)
                         writer.add_histogram('grads/' + tag, value.grad.data.cpu().numpy(), global_step)
-                    val_score = eval_net(net, val_loader, device, regularizer)
+                    val_score = eval_net(net, val_loader, device, regularizer, epoch)
                     # scheduler.step(val_score)
                     writer.add_scalar('learning_rate', optimizer.param_groups[0]['lr'], global_step)
 
-                    if net.n_classes > 1:
+                    # if net.n_classes > 1:
+                    if True:
                         logging.info('Validation L1 loss: Total: {}, Mask: {}, Recon: {}, Batch IoU: {}'.format(val_score[0], val_score[1], val_score[2], val_score[3]))
                         writer.add_scalar('Loss/test', val_score[0], global_step)
                         writer.add_scalar('Recon/test', val_score[1], global_step)
@@ -181,10 +200,11 @@ def train_net(args,
                         writer.add_scalar('IoU/test', val_score[3], global_step)
 
                     writer.add_images('images', imgs, global_step)
-                    if net.n_classes == 1:
+                    # if net.n_classes == 1:
+                    if True:
                         writer.add_images('masks/true', recon_img, global_step)
                         writer.add_images('masks/pred', torch.sigmoid(pred_recon_img) > 0.5, global_step)
-                    save_cp = val_score[3] > 0.20
+                    # save_cp = val_score[3] > 0.20
 
         if save_cp:
             try:
@@ -229,6 +249,8 @@ def get_args():
                         help='Number of classes in the dataset. If 1 or 2, use 1. Else use the number of classes.', dest='classes')
     parser.add_argument('-rd', '--rootDir', metavar='RD', type=str, default=Path().resolve().parent,
                         help='Root Directory for dataset', dest='rd')
+    parser.add_argument('-ir', '--imageRes', dest='im_res', type=int, default=224,
+                        help='Input Image resolution')
 
     return parser.parse_args()
 
@@ -257,7 +279,10 @@ if __name__ == '__main__':
     #   - For 1 class and background, use n_classes=1
     #   - For 2 classes, use n_classes=1
     #   - For N > 2 classes, use n_classes=N
-    net = UNet(n_channels=3, n_classes=args.classes, bilinear=True)
+    # net = UNet(n_channels=3, n_classes=args.classes, bilinear=True)
+    # net = torch.hub.load('pytorch/vision:v0.10.0', 'fcn_resnet50', pretrained=False)
+    net = create_model()
+    # print(net)
 
     # Multi-GPU
 
@@ -266,10 +291,16 @@ if __name__ == '__main__':
     # net = nn.DataParallel(UNet(n_channels=3, n_classes=args.classes, bilinear=True))
 
 
+    # logging.info(f'Network:\n'
+    #              f'\t{net.n_channels} input channels\n'
+    #              f'\t{net.n_classes} output channels (classes)\n'
+    #              f'\t{"Bilinear" if net.bilinear else "Transposed conv"} upscaling')
+
     logging.info(f'Network:\n'
-                 f'\t{net.n_channels} input channels\n'
-                 f'\t{net.n_classes} output channels (classes)\n'
-                 f'\t{"Bilinear" if net.bilinear else "Transposed conv"} upscaling')
+                 f'\t{3} input channels\n'
+                 f'\t{21} output channels (classes)\n'
+                 # f'\t{"Bilinear" if net.bilinear else "Transposed conv"} upscaling')
+                 )
 
     if args.load:
         net.load_state_dict(
@@ -278,7 +309,10 @@ if __name__ == '__main__':
         logging.info(f'Model loaded from {args.load}')
 
     net.to(device=device)
-    torchsummary.summary(net, input_size=(3, 160, 160))
+    # torchsummary.summary(net, input_size=(3, 160, 160))
+    # torchsummary.summary(net.backbone, input_size=(3, args.im_res, args.im_res))
+    # torchsummary.summary(net.classifier, input_size=(2048, args.im_res, args.im_res))
+    # torchsummary.summary(net.aux_classifier, input_size=(1024, args.im_res, args.im_res))
     # faster convolutions, but more memory
     # cudnn.benchmark = True
 
