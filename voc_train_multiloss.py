32f70c59450bd18e28bdf840f95fc86cb7ba289d
Omkar Kulkarni <omkar@Omkars-MacBook-Pro.local>
2024-07-16 20:35:26 -0400

    vocvis

--- voc_train_multiloss.py
+++ voc_train_multiloss.py
@@ -17,8 +17,8 @@ from voc_eval_multiloss import eval_net
 from unet import UNet
 
 from torch.utils.tensorboard import SummaryWriter
-# from utils.pascalVOC_multiloss_pl import PascalVOCDataset
-from utils.pascalVOC_multiloss import PascalVOCDataset
+from utils.pascalVOC_multiloss_pl import PascalVOCDataset
+# from utils.pascalVOC_multiloss import PascalVOCDataset
 # from utils.petsReconDataset_multiloss import PetsReconDataset
 from utils.percLoss import percLoss
 from torch.utils.data import DataLoader, random_split
