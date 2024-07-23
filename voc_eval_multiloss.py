1b9e455da3266cc600d01b298c3a5daee7bef575
omkar-umbc <omkark1@umbc.edu>
2024-07-23 17:33:39 -0400

    voc

--- voc_eval_multiloss.py
+++ voc_eval_multiloss.py
@@ -53,8 +53,9 @@ def eval_net(net, loader, device, regularizer, epoch):
                 #     mean_batch_iou += single_iou
 
                 # batch_iou = multiclass_jaccard_index(pred_masks, amax_true_masks, num_classes=21)
-                batch_iou = multiclass_jaccard_index(pred_masks, true_masks, num_classes=21, average=None)
-                print(batch_iou)
+                test_iou = multiclass_jaccard_index(pred_masks, true_masks, num_classes=21, average=None)
+                print(test_iou)
+                batch_iou = multiclass_jaccard_index(pred_masks, true_masks, num_classes=21)
                 # print(batch_iou, mean_batch_iou / len(pred_masks), mean_batch_iou)
 
                 # iou += (mean_batch_iou / len(pred_masks))
