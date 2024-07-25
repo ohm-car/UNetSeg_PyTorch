37686d09f1be672533bf7ab7b9e7f8d6c3b1b82a
omkar-umbc <omkark1@umbc.edu>
2024-07-24 21:56:00 -0400

    voc

--- voc_train_multiloss.py
+++ voc_train_multiloss.py
@@ -49,7 +49,7 @@ def create_model():
                  nn.Conv2d(512, 3, kernel_size=(1, 1), stride=(1, 1)),
                  nn.Sigmoid())
     model.aux_classifier = aux
-    model.classifier.append(nn.Softmax(dim=0))
+    model.classifier.append(nn.Softmax(dim=3))
     return model
 
 
