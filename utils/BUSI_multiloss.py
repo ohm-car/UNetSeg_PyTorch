e9a0ec1642fcbf2d7c721d6b29d17523e081d19c
omkar-umbc <omkark1@umbc.edu>
2024-10-08 18:15:40 -0400

    blob

--- utils/BUSI_multiloss.py
+++ utils/BUSI_multiloss.py
@@ -17,11 +17,12 @@ from skimage.feature import blob_dog, blob_log, blob_doh
 from skimage.morphology import square, erosion, binary_erosion
 from skimage.color import rgb2gray
 from skimage.io import imread
+from skimage.transform import resize
 
 """A custom dataset loader object. This dataset returns the same labels as the input"""
 
 class BUSIDataset(Dataset):
-    def __init__(self, root_dir, im_res = 224, scale=1, mask_suffix='', preload = False):
+    def __init__(self, root_dir, threshold = 50, im_res = 224, scale=1, mask_suffix='', preload = True):
 
         self.main_dir = os.path.join(root_dir, 'Datasets/Dataset_BUSI_with_GT/')
         # self.imgs_dir = os.path.join(root_dir, 'Datasets/VOCdevkit/VOC2012/JPEGImages/')
@@ -33,13 +34,15 @@ class BUSIDataset(Dataset):
         self.im_res = (im_res, im_res)
         self.scale = scale
         self.mask_suffix = mask_suffix
+        self.threshold = threshold
 
         self.preload = preload
-
-        if self.preload:
-            self.images, self.masks, self.eroded_masks, self.percs = self.load_data(self.file_list)
-
         self.transform = transforms.Compose([transforms.PILToTensor()])
+        if self.preload:
+            self.images, self.masks, self.eroded_masks, self.percs = self.load_data()
+            logging.info(f'Loaded dataset with {len(self.file_list)} examples')
+
+        # transform = transforms.Compose([transforms.PILToTensor()])
         # self.percsDict = self.getPercsDict(percs_dir)
 
         assert 0 < scale <= 1, 'Scale must be between 0 and 1'
@@ -50,12 +53,21 @@ class BUSIDataset(Dataset):
 
     def load_data(self):
 
-        for filename in self.file_list:
-            img = load_image(filename)
-            mask = load_image_masks(filename)
-            eroded_mask = eroded_image_masks(filename)
+        images, masks, eroded_masks, percs = list(), list(), list(), list()
 
-        return None
+        for filename in self.file_list:
+            # print(filename)
+            img = self.load_image(filename)
+            mask = self.load_image_masks(filename)
+            eroded_mask = self.eroded_image_masks(filename)
+            perc = self.get_perc(mask)
+
+            images.append(img)
+            masks.append(mask)
+            eroded_masks.append(eroded_mask)
+            percs.append(perc)
+
+        return images, masks, eroded_masks, percs
 
     def load_image(self, filename):
 
@@ -71,11 +83,12 @@ class BUSIDataset(Dataset):
     def load_image_masks(self, filename):
 
         mask_file = glob(self.main_dir + filename + '_mask' + '*')
-        og_masks = list()
         mask = torch.unsqueeze(torch.zeros(self.im_res), dim=0)
         for mf in mask_file:
             #Mask as torch Tensor
             M = Image.open(mf)
+            if M.mode != '1':
+                M = M.convert(mode = '1')
             M = self.preprocess_mask(M, self.transform)
             mask += M
             
@@ -84,8 +97,37 @@ class BUSIDataset(Dataset):
 
     def eroded_image_masks(self, filename):
 
-        return eroded_image_masks
+        mask_file = glob(self.main_dir + filename + '_mask' + '*')
+        tp = imread(mask_file[0])
+        # eroded_mask = np.expand_dims(np.zeros(self.im_res), axis=0)
+        eroded_mask = torch.unsqueeze(torch.zeros(self.im_res), dim=0)
+        for mf in mask_file:
+            #Mask as np array
+            M = imread(mf, as_gray = True)
+            #resize np mask
+            M = resize(M, self.im_res)
+            e_M = self.eroded_mask(M)
+            eroded_mask += e_M
 
+        return eroded_mask
+
+    def eroded_mask(self, mask):
+
+        # o_pixels = np.sum(mask)
+        e_mask = mask
+        pixels = np.sum(e_mask)
+        # print(threshold, pixels)
+        while pixels >= self.threshold:
+            e_mask_t = erosion(e_mask, np.ones((3,3)))
+            pixels = np.sum(e_mask_t)
+            
+            if pixels != 0:
+                e_mask = e_mask_t
+            
+        # print(pixels)
+        
+        return torch.from_numpy(e_mask)
+        # return e_mask
 
 
     def __len__(self):
@@ -213,54 +255,32 @@ class BUSIDataset(Dataset):
     #     return out
 
     def __getitem__(self, i):
-        idx = self.file_list[i]
-        # print(self.imgs_dir, self.masks_dir, self.mask_suffix)
+        
+        if self.preload:
 
-        img_file = glob(self.main_dir + idx + '.*')
+            idx = self.file_list[i]
+            T = self.images[i]
+            M = self.masks[i]
+            Mc = self.eroded_masks[i]
+            P = self.percs[i]
 
-        mask_file = glob(self.main_dir + idx + '_mask' + '.*')
+            return {
+                'image_ID': idx,
+                'image': T,
+                'reconstructed_image': T,
+                'mask': M,
+                'comp_mask': Mc,
+                'mask_perc': P
+            }
+        else:
 
-        assert len(img_file) == 1, \
-            f'Either no image or multiple images found for the ID {i}: {img_file}'
+            idx = self.file_list[i]
+            T = self.load_image(idx)
+            M = self.load_image_masks(idx)
+            Mc = self.eroded_image_masks(idx)
+            P = self.get_perc(M)
 
-        assert len(mask_file) == 1, \
-            f'Either no mask or multiple masks found for the ID {i}: {mask_file}'
-
-        T = Image.open(img_file[0])
-        if T.mode != 'RGB':
-            T = T.convert(mode = 'RGB')
-
-        T = self.preprocess(T, self.transform)
-        # print("Image tensor type ", T)
-
-
-        M = Image.open(mask_file[0])
-        # print(M.mode)
-        # assert M.mode == 'L' or M.mode == '1', \
-        #     f'Error with file {mask_file}'
-
-        M = self.preprocess_mask(M, self.transform)
-        # print("Mask tensor type ", M)
-        # print("Mask shape: ", M.shape)
-
-        # M = M - ((M == 255) * 255)
-        assert torch.max(M) == 1.0 and torch.min(M) == 0,\
-            f'Check mask file'
-
-        P = self.get_perc(M)
-        # print(P)
-
-        # Mp = M1 + M3
-        # print(M.shape, torch.squeeze(M).shape, M.dtype)
-        # Mp = torch.permute(one_hot(torch.squeeze(M), num_classes = self.num_classes), (2, 0, 1))
-        # Mp = torch.permute(torch.squeeze(M), (2, 0, 1))
-        # Mp = torch.squeeze(M)
-        Mc = self.gen_partial_mask(M)
-
-        assert Mc.size() == M.size(), \
-            f'Shapes mismatch {Mc.size()} != {M.size()}'
-
-        return {
+            return {
             'image_ID': idx,
             'image': T,
             'reconstructed_image': T,
@@ -268,3 +288,60 @@ class BUSIDataset(Dataset):
             'comp_mask': Mc,
             'mask_perc': P
         }
+
+    # def __getitem__(self, i):
+    #     idx = self.file_list[i]
+    #     # print(self.imgs_dir, self.masks_dir, self.mask_suffix)
+
+    #     img_file = glob(self.main_dir + idx + '.*')
+
+    #     mask_file = glob(self.main_dir + idx + '_mask' + '.*')
+
+    #     assert len(img_file) == 1, \
+    #         f'Either no image or multiple images found for the ID {i}: {img_file}'
+
+    #     assert len(mask_file) == 1, \
+    #         f'Either no mask or multiple masks found for the ID {i}: {mask_file}'
+
+    #     T = Image.open(img_file[0])
+    #     if T.mode != 'RGB':
+    #         T = T.convert(mode = 'RGB')
+
+    #     T = self.preprocess(T, self.transform)
+    #     # print("Image tensor type ", T)
+
+
+    #     M = Image.open(mask_file[0])
+    #     # print(M.mode)
+    #     # assert M.mode == 'L' or M.mode == '1', \
+    #     #     f'Error with file {mask_file}'
+
+    #     M = self.preprocess_mask(M, self.transform)
+    #     # print("Mask tensor type ", M)
+    #     # print("Mask shape: ", M.shape)
+
+    #     # M = M - ((M == 255) * 255)
+    #     assert torch.max(M) == 1.0 and torch.min(M) == 0,\
+    #         f'Check mask file'
+
+    #     P = self.get_perc(M)
+    #     # print(P)
+
+    #     # Mp = M1 + M3
+    #     # print(M.shape, torch.squeeze(M).shape, M.dtype)
+    #     # Mp = torch.permute(one_hot(torch.squeeze(M), num_classes = self.num_classes), (2, 0, 1))
+    #     # Mp = torch.permute(torch.squeeze(M), (2, 0, 1))
+    #     # Mp = torch.squeeze(M)
+    #     Mc = self.gen_partial_mask(M)
+
+    #     assert Mc.size() == M.size(), \
+    #         f'Shapes mismatch {Mc.size()} != {M.size()}'
+
+    #     return {
+    #         'image_ID': idx,
+    #         'image': T,
+    #         'reconstructed_image': T,
+    #         'mask': M,
+    #         'comp_mask': Mc,
+    #         'mask_perc': P
+    #     }
