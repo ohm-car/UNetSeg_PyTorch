7cae69f8577375a221006ce0fc0caa9acde8e38c
omkar-umbc <omkark1@umbc.edu>
2024-10-29 17:58:28 -0400

    blob

--- busi.sh
+++ busi.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 #SBATCH --job-name=busi_mlrseg
+#SBATCH --array=0-7
 #SBATCH --mail-user=omkark1@umbc.edu
 #SBATCH --mem=32G
-#SBATCH --gres=gpu:1
+#SBATCH --gres=gpu:8
 #SBATCH --time=10-12:00:00
 #SBATCH --constraint=rtx_2080
 #SBATCH --output=output_%j.log
@@ -18,7 +19,11 @@ source activate unet
 # cp /nfs/ada/oates/users/omkark1/Thesis_Work/Datasets.zip /scratch/$SLURM_JOBID
 # unzip -q /scratch/$SLURM_JOBID/Datasets.zip -d /scratch/$SLURM_JOBID
 
-python /nfs/ada/oates/users/omkark1/Thesis_Work/UNetSeg_PyTorch/busi_optuna_train_multiloss.py -pl=True -e=160 -b=36 -ir=224 -th=50
+# python /nfs/ada/oates/users/omkark1/Thesis_Work/UNetSeg_PyTorch/busi_optuna_train_multiloss.py -pl=True -e=160 -b=36 -ir=224 -th=50
+
+th=$(echo "0.04 * $SLURM_ARRAY_TASK_ID" | bc)
+
+echo "The threshold is: $th"
 
 mv output_$SLURM_JOBID.log /nfs/ada/oates/users/omkark1/Thesis_Work/UNetSeg_PyTorch/outfiles/busi/optuna/output_$DT.log
 mv output_$SLURM_JOBID.err /nfs/ada/oates/users/omkark1/Thesis_Work/UNetSeg_PyTorch/outfiles/busi/optuna/output_$DT.err
