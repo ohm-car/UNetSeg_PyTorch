9f73188e7d0995ec7d9dfcb98bfaa7cf534cfbcb
omkar-umbc <omkark1@umbc.edu>
2024-11-03 20:58:06 -0500

    busi

--- busi.sh
+++ busi.sh
@@ -37,7 +37,7 @@ echo "The threshold is: ${th[$SLURM_ARRAY_TASK_ID]}"
 concatenated_id="${SLURM_ARRAY_JOB_ID}_${SLURM_ARRAY_TASK_ID}"
 echo "Concatenated ID: $concatenated_id"
 
-python /nfs/ada/oates/users/omkark1/Thesis_Work/UNetSeg_PyTorch/busi_optuna_train_multiloss.py -pl=True -e=160 -b=36 -ir=224 -th=$th -j=$concatenated_id
+python /nfs/ada/oates/users/omkark1/Thesis_Work/UNetSeg_PyTorch/busi_optuna_train_multiloss.py -pl=True -e=160 -b=32 -ir=224 -th=$th -j=$concatenated_id
 # mv output_$SLURM_JOBID.log /nfs/ada/oates/users/omkark1/Thesis_Work/UNetSeg_PyTorch/outfiles/busi/optuna/output_$DT.log
 # mv output_$SLURM_JOBID.err /nfs/ada/oates/users/omkark1/Thesis_Work/UNetSeg_PyTorch/outfiles/busi/optuna/output_$DT.err
 
