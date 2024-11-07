044fa5e631ea44182debcbd4176fa8f625c225e8
Omkar Kulkarni <omkar@Omkars-MacBook-Pro.local>
2024-11-07 10:04:03 -0500

    busi

--- busi_array.sh
+++ busi_array.sh
@@ -5,7 +5,6 @@
 #SBATCH --mail-user=omkark1@umbc.edu
 #SBATCH --mem=32G
 #SBATCH --gres=gpu:1
-#SBATCH --nodelist=g06,g07
 #SBATCH --time=10-12:00:00
 #SBATCH --constraint=rtx_6000
 #SBATCH --output=outfiles/final/busi/output_%A_%a.log
