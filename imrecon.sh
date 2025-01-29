bcf18746427f252a536b29316cb6fe157a3b8b32
omkar-umbc <omkark1@umbc.edu>
2025-01-28 22:14:41 -0500

    pets fl

--- imrecon.sh
+++ imrecon.sh
@@ -5,9 +5,9 @@
 #SBATCH --mem=32G
 #SBATCH --gres=gpu:1
 #SBATCH --time=10-12:00:00
-#SBATCH --constraint=rtx_2080
-#SBATCH --output=output_%j.log
-#SBATCH --error=output_%j.err
+#SBATCH --constraint=rtx_6000
+#SBATCH --output=outfiles/final/pets/output_%j.log
+#SBATCH --error=outfiles/final/pets/output_%j.err
 
 DT=`date +"%m-%d_%H-%M"`
 
@@ -19,20 +19,20 @@ source activate unet
 # unzip -q /scratch/$SLURM_JOBID/Datasets.zip -d /scratch/$SLURM_JOBID
 
 # python /nfs/ada/oates/users/omkark1/Thesis_Work/UNetSeg_PyTorch/train_multiloss.py -rd=/scratch/$SLURM_JOBID -b=160 -e=60 -sf=10 -rw=0.1 -r='omkar'
-# python /nfs/ada/oates/users/omkark1/Thesis_Work/UNetSeg_PyTorch/optuna_train_multiloss.py -b=72 -e=60 -ir=224
+python /nfs/ada/oates/users/omkark1/Thesis_Work/UNetSeg_PyTorch/pets_optuna_train_multiloss.py -b=72 -e=60 -ir=224 -j=$SLURM_JOBID
 # python /nfs/ada/oates/users/omkark1/Thesis_Work/UNetSeg_PyTorch/voc_train_segA.py -e=100 -b=72 -ir=224
 # python /nfs/ada/oates/users/omkark1/Thesis_Work/UNetSeg_PyTorch/ov_optuna_voc_train_multiloss.py -b=72 -e=80
 # python /nfs/ada/oates/users/omkark1/Thesis_Work/UNetSeg_PyTorch/voc_train_multiloss.py -b=72 -e=800
 # python /nfs/ada/oates/users/omkark1/Thesis_Work/UNetSeg_PyTorch/busi_train_multiloss.py -e=200 -b=72 -ir=224
 # python /nfs/ada/oates/users/omkark1/Thesis_Work/UNetSeg_PyTorch/pets_train_seg.py -b=64 -e=60 -ir=224
 # python /nfs/ada/oates/users/omkark1/Thesis_Work/UNetSeg_PyTorch/busi_train_segA.py -b=36 -e=100 -ir=224
-python /nfs/ada/oates/users/omkark1/Thesis_Work/UNetSeg_PyTorch/busi_optuna_train_multiloss.py -pl=True -e=160 -b=36 -ir=224 -th=50
+# python /nfs/ada/oates/users/omkark1/Thesis_Work/UNetSeg_PyTorch/busi_optuna_train_multiloss.py -pl=True -e=160 -b=36 -ir=224 -th=50
 
 # mv output_$SLURM_JOBID.log /nfs/ada/oates/users/omkark1/Thesis_Work/UNetSeg_PyTorch/outfiles/voc/output_$DT.log
 # mv output_$SLURM_JOBID.err /nfs/ada/oates/users/omkark1/Thesis_Work/UNetSeg_PyTorch/outfiles/voc/output_$DT.err
 
-mv output_$SLURM_JOBID.log /nfs/ada/oates/users/omkark1/Thesis_Work/UNetSeg_PyTorch/outfiles/busi/optuna/output_$DT.log
-mv output_$SLURM_JOBID.err /nfs/ada/oates/users/omkark1/Thesis_Work/UNetSeg_PyTorch/outfiles/busi/optuna/output_$DT.err
+# mv output_$SLURM_JOBID.log /nfs/ada/oates/users/omkark1/Thesis_Work/UNetSeg_PyTorch/outfiles/busi/optuna/output_$DT.log
+# mv output_$SLURM_JOBID.err /nfs/ada/oates/users/omkark1/Thesis_Work/UNetSeg_PyTorch/outfiles/busi/optuna/output_$DT.err
 
 # mv output_$SLURM_JOBID.log /nfs/ada/oates/users/omkark1/Thesis_Work/UNetSeg_PyTorch/outfiles/busi/segmentation/output_$DT.log
 # mv output_$SLURM_JOBID.err /nfs/ada/oates/users/omkark1/Thesis_Work/UNetSeg_PyTorch/outfiles/busi/segmentation/output_$DT.err
