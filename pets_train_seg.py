8117c617a1d43f839ebeea055ea21c66f1d7aa22
omkar-umbc <omkark1@umbc.edu>
2025-07-30 21:10:47 -0400

    NM

--- pets_train_seg.py
+++ pets_train_seg.py
@@ -20,7 +20,7 @@ from architectures.pets.unet_model_seg import UNet
 from torch.utils.tensorboard import SummaryWriter
 # from utils.pascalVOC_multiloss_pl import PascalVOCDataset
 # from utils.pascalVOC_multiloss import PascalVOCDataset
-from utils.petsReconDataset_multiloss_pl import PetsReconDataset
+from utils.petsReconDataset_multiloss import PetsReconDataset
 from utils.percLoss import percLoss
 from torch.utils.data import DataLoader, random_split
 
